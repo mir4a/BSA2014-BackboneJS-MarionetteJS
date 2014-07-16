1827ed49f262734fcef2a7e58386d7dbba652e76
Miroslav Martynov <marty.mircha@gmail.com>
2014-07-16 16:22:34 +0300

    Added button 'new video'

--- public/app/javascripts/FilmCollectionView.js
+++ public/app/javascripts/FilmCollectionView.js
@@ -1,6 +1,12 @@
 var FilmCollectionView = Backbone.View.extend({
 	el: '#films-container',
+
+  events: {
+    'click #add-new': 'addNewFilm'
+  },
+
 	initialize: function(){
+    this.createButtonAdd();
 		this.collection.on('add', this.renderNewFilm, this);
 	},
 
@@ -9,10 +15,22 @@ var FilmCollectionView = Backbone.View.extend({
 			model: model
 		});
 		this.$el.append(view.$el);
-	}
+	},
+
+  createButtonAdd: function() {
+    this.$el.append('<button id="add-new">Add new</button>');
+  },
+
+  addNewFilm: function(){
+    console.log('added new film');
+  },
+
+  addNewFilmForm: function() {
+    console.log('there should be a new form view');
+  }
+
 });
 
 var filmsView = new FilmCollectionView({
 	collection: films
 });
-
